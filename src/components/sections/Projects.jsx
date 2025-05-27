893bc1fc71281ecd87d41e755ef04b760a4abf84
athexshu408 <atharvingale408@gmail.com>
2025-05-27 13:29:43 +0530

    project changes

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -17,7 +17,7 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                 <h3 className="text-gray-400 mb-4">Fleet Managment System </h3>
                 <p>
-                     Created and maintained fleet managment backend and frontend for web application  
+                   Developed a comprehensive Fleet Management System designed to help organizations efficiently monitor, manage, and optimize their vehicle fleets. The system provides real-time vehicle tracking, maintenance scheduling, driver management, and analytics dashboards to improve fleet utilization and reduce operational costs.
                 </p>
                 <div>
                     {["React" , "Node.js" ,"Postgres","leaflet"].map((tech,key)=>{
@@ -31,9 +31,9 @@ export const Projects = () => {
                 </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                <h3 className="text-gray-400 mb-4">Fleet Managment System </h3>
+                <h3 className="text-gray-400 mb-4">NavXperience</h3>
                 <p>
-                     Created and maintained fleet managment backend and frontend for web application  
+                    NavXperience” is an interactive web application focused on path navigation and real-time visualization of LIDAR data. The application allows users to design navigation paths on a map interface, visualize sensor (LIDAR) data overlays, and interact with dynamic spatial information for enhanced situational awareness.
                 </p>
                 <div>
                     {["React" , "Node.js" ,"Postgres","leaflet"].map((tech,key)=>{
@@ -47,9 +47,9 @@ export const Projects = () => {
                 </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                <h3 className="text-gray-400 mb-4">Fleet Managment System </h3>
+                <h3 className="text-gray-400 mb-4">Pizza delivery app </h3>
                 <p>
-                     Created and maintained fleet managment backend and frontend for web application  
+                    Developed a full-featured Pizza Delivery web application that allows customers to browse menus, place orders, customize pizzas, and securely pay online using Razorpay. The application manages user authentication, order tracking, and admin dashboards for managing inventory and orders.
                 </p>
                 <div>
                     {["React" , "Node.js" ,"Postgres","leaflet"].map((tech,key)=>{
@@ -63,9 +63,9 @@ export const Projects = () => {
                 </div>
             </div>
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                <h3 className="text-gray-400 mb-4">Fleet Managment System </h3>
+                <h3 className="text-gray-400 mb-4">Blog app</h3>
                 <p>
-                     Created and maintained fleet managment backend and frontend for web application  
+                    Developed a robust Blog Application that allows users to create, read, update, and delete blog posts while managing user authentication and comments. The platform supports rich text editing, user profiles, and responsive design for seamless access across devices.
                 </p>
                 <div>
                     {["React" , "Node.js" ,"Postgres","leaflet"].map((tech,key)=>{
